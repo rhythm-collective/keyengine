c8e42cb881af7106ac2c3b13ec12900297301524
Adam Bryant <hello@adambryant.ca>
2019-09-04 18:09:32 -0700

    Example svg and css animation.

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Game from "./components/game";
 import Functional from "./components/functional";
+import sample from "./assets/sample.svg";
+import "./app.css";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={sample}
+          className="App-logo"
+          alt="logo"
+          height="160"
+          width="160"
+        />
+      </header>
       <Game />
       <Functional />
     </div>
